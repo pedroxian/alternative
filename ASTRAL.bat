235f4a8757898dab3d23c3cf54b1cdc4b9a90637
Petró Zoltán <44950875+pedroxian@users.noreply.github.com>
2019-07-10 13:02:58 +0200

    boot revenger
    chops riff

---
+++ ASTRAL.bat
@@ -0,0 +1,7 @@
+>>loading load c: boot.mgr write.change write
+>>piller.tab write chauls.sdf
+>>run operation set _self.ping
+>>vaulty.grub -endless.os
+>>mute:chill write foulsch.bit
+>>{sach set write grun.sd eff _lite.grub
+mode chart :revell mod.get api}
