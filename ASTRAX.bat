2036887a20500a357c5b621e2412267ed800bede
Petró Zoltán <44950875+pedroxian@users.noreply.github.com>
2019-07-10 12:33:52 +0200

    clove rune art
    bat  extracter

---
+++ ASTRAX.bat
@@ -0,0 +1,10 @@
+>>grow.write write avalanch.dll
+
+>>{expose chans write glav.dll
+	such:chage write.dav
+
+		write change}
+
+>>pause let.give set
+>>write apox.red.run elpo.x write six.6
+>>pesox.load load a: drive.change
