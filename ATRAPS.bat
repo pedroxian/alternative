3a4ee17700c70325cb442fe04a4ba0950ef2859e
Petró Zoltán <44950875+pedroxian@users.noreply.github.com>
2019-07-11 21:32:44 +0200

    via hungary change
    solid state

---
+++ ATRAPS.bat
@@ -0,0 +1,10 @@
+>>write opx.ass -desting besh.rut_leaf
+>>drt.rop section direct write {dr.direct 
+		send.loaf .big-f ruit}
+
+>>write rauch.afi -run.d.root /fad.5
+>>{solid est f.tr-rops
+	rush.proof _ruit.exec}
+
+>>write rash.adg -mesh {frog -build.tr.rap.nation pld
+		mash.klonde r.p-r refeder.com}
