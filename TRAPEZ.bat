ce2eaaa358205e16f34caa000247e1083719f3c8
Petró Zoltán <44950875+pedroxian@users.noreply.github.com>
2019-07-11 22:22:15 +0200

    multi compressor
    failed compress

---
+++ TRAPEZ.bat
@@ -0,0 +1,6 @@
+>>setting right.everth.ofg "6
+>>write comet.hga
+>>{slot.efg -sotig _visual.f2E2R3}
+>>write orga.wad
+>>{flow.wait -ff1 .yape rott.g
+	effort.vda G4.2 _slet.of even.rune .else -off}
